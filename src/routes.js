bdd77d040e161c52d49f6ea475f5224feed9e5b2
Boris Musatov <boris.musatov@gmail.com>
2015-09-01 00:34:34 +0200

    not found page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Route, DefaultRoute } from 'react-router'
+import { Route, DefaultRoute, NotFoundRoute } from 'react-router'
 
 import Home from './components/home'
 import Repo from './components/repo'
 import Search from './components/search'
 
+const NotFound = React.createClass({
+  render() {
+    return (
+      <div className='not-found'>
+        <h2>Give it another shot</h2>
+      </div>
+    )
+  }
+})
+
 export default (
   <Route path='/' handler={ Home }>
     <DefaultRoute handler={ Search } />
     <Route name='repo' path='/repo/:owner/:name' handler={ Repo } />
+    <NotFoundRoute handler={ NotFound } />
   </Route>
 );
